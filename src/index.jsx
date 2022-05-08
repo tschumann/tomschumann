66809d58eb5762dec6be80f1303ac0c77a2890af
tschumann <schumann.tom@gmail.com>
2022-05-08 13:49:59 +1000

    Added more projects.

--- src/index.jsx
+++ src/index.jsx
@@ -31,26 +31,40 @@ const projects = [
 	{
 		'id': 1,
 		'name': 'Sandbot',
-		'body': 'Sandbot is a Half-Life engine multiplayer bot. It is based on <a href="http://hpb-bot.bots-united.com/hpb_bot.html" target="_blank">HPB bot</a> and has been updated with support for more games and mods and to compile with modern versions of Visual C++ and gcc. It is written in C and C++ for Windows and Linux.',
+		'body': 'Sandbot is a Half-Life engine multiplayer bot. It is based on <a href="http://hpb-bot.bots-united.com/hpb_bot.html" target="_blank">HPB bot</a> and has been updated with support for more games and mods; support for modern versions of Visual C++ and gcc and tests. It is written in C and C++ for Windows and Linux.',
 		'source_link': 'https://github.com/tschumann/sandbot',
 		'language': 'cpp'
 	},
 	{
 		'id': 2,
+		'name': 'sandbot-wptgen',
+		'body': 'sandbot-wptgen is a Sandbot waypoint generator. It is based on <a href="http://hpb-bot.bots-united.com/bsp_tool.html" target="_blank">BSP_tool</a> and has been updated with support for more games and mods; support for modern versions of Visual C++ and gcc and tests. It is written in C and C++ for Windows and Linux.',
+		'source_link': 'https://github.com/tschumann/sandbot-wptgen',
+		'language': 'cpp'
+	},
+	{
+		'id': 3,
 		'name': 'ktext',
 		'body': 'ktext is a .pdf manipulator. It exposes some of the functionality of <a href="https://itextpdf.com/en" target="_blank">iText</a> through a command line tool. It is written in Kotlin and Java.',
 		'source_link': 'https://github.com/tschumann/ktext',
 		'language': 'kt'
 	},
 	{
-		'id': 3,
+		'id': 4,
 		'name': 'scrape',
 		'body': 'scrape a website downloader. It recursively downloads all linked pages of a website. It is written in Python.',
 		'source_link': 'https://github.com/tschumann/scrape',
 		'language': 'py'
 	},
 	{
-		'id': 4,
+		'id': 5,
+		'name': 'Capitle',
+		'body': 'Capitle is <a href="http://capitle.tomschumann.com/" target="_blank">a capital city guessing game</a>. It is written in pure Javascript.',
+		'source_link': 'https://github.com/tschumann/capitle',
+		'language': 'js'
+	}
+	{
+		'id': 6,
 		'name': 'tomschumann.com',
 		'body': "tomschumann.com is this website. It has a bit of information about me and some projects I've worked on. It is written in Javascript using React.",
 		'source_link': 'https://github.com/tschumann/tomschumann',
