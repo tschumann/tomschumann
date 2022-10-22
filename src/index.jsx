90132f6bfcc5404eac31c921e52704bf9c9c7017
Tom Schumann <schumann.tom@gmail.com>
2022-10-22 17:04:02 +1000

    Added new post and added tags to posts.

--- src/index.jsx
+++ src/index.jsx
@@ -9,32 +9,44 @@ const posts = [
 		'id': 1,
 		'title': 'Show hidden files in Ubuntu',
 		'time': '2021-12-31 09:21:00',
-		'body': 'Ctrl+H in Files.'
+		'body': 'Ctrl+H in Files.',
+		'tags': ['linux']
 	},
 	{
 		'id': 2,
 		'title': 'Transferring files to and from an Android phone on Windows',
 		'time': '2021-12-31 10:39:00',
-		'body': 'On older versions of Android if you slide down the menu, File transfer will be selected by default but may not work unless you select Photo transfer (or some other option) then reselect File transfer. On newer versions of Android No data transfer will be selected by default but you can change to File transfer. The phone will then show up under My Computer/This PC in File Explorer.'
+		'body': 'On older versions of Android if you slide down the menu, File transfer will be selected by default but may not work unless you select Photo transfer (or some other option) then reselect File transfer. On newer versions of Android No data transfer will be selected by default but you can change to File transfer. The phone will then show up under My Computer/This PC in File Explorer.',
+		'tags': ['android']
 	},
 	{
 		'id': 3,
 		'title': 'Counting lines of actual code in Visual Studio',
 		'time': '2021-12-31 10:43:00',
 		// the \\ is actually \ so copy what gets rendered, not what is here
-		'body': 'Do a text search using the following regex: ^~(:Wh@//.+)~(:Wh@\\{:Wh@)~(:Wh@\\}:Wh@)~(:Wh@\\#).+'
+		'body': 'Do a text search using the following regex: ^~(:Wh@//.+)~(:Wh@\\{:Wh@)~(:Wh@\\}:Wh@)~(:Wh@\\#).+',
+		'tags': ['visual-studio']
 	},
 	{
 		'id': 4,
 		'title': 'Connect to a Windows Shared Folder from Ubuntu',
 		'time': '2022-06-18 08:46:00',
-		'body': 'In Ubuntu, open the file explorer and go to File -> Connect to Server. Server is the computer\'s IP address, Type is Windows share, Share can be left blank, and Folder is the name of the folder you\'re connecting to. Domain name can be left blank (it can work it out? or is it the workgroup name?), User name is the Windows username and Password is the Windows password.'
+		'body': 'In Ubuntu, open the file explorer and go to File -> Connect to Server. Server is the computer\'s IP address, Type is Windows share, Share can be left blank, and Folder is the name of the folder you\'re connecting to. Domain name can be left blank (it can work it out? or is it the workgroup name?), User name is the Windows username and Password is the Windows password.',
+		'tags': ['linux']
 	},
 	{
 		'id': 5,
 		'title': 'Building a .dll or .so with Fortran Package Manager',
 		'time': '2022-10-01 15:34:00',
-		'body': 'As at the time of writing at least (latest Fortran Package Manager is v0.6.0), fpm cannot build a .dll or .so file. fpm will build all of the object files, so all that is required is to link the .o files into a binary.<br/>On Windows: gfortran -static -shared build\\*.o -o output.dll<br/>On Linux: gfortran -static -shared build/*.o -o output.so<br/>If you\'re linking 32-bit code also add -m32 to the gfortran parameters.'
+		'body': 'As at the time of writing at least (latest Fortran Package Manager is v0.6.0), fpm cannot build a .dll or .so file. fpm will build all of the object files, so all that is required is to link the .o files into a binary.<br/>On Windows: gfortran -static -shared build\\*.o -o output.dll<br/>On Linux: gfortran -static -shared build/*.o -o output.so<br/>If you\'re linking 32-bit code also add -m32 to the gfortran parameters.',
+		'tags': ['fortran']
+	},
+	{
+		'id': 6,
+		'title': 'Debugging a .dll with Visual Studio',
+		'time': '2022-10-22 16:58:00',
+		'body': 'In Visual Studio go to Debug -> Attach to Process and choose the running process that the .dll has been loaded into.',
+		'tags': ['visual-studio']
 	}
 ];
 
